a67eff62780f92af5f9a50bee63f21275a9ea152
BrokenDev375 <faonly2dgirls@gmail.com>
2025-09-20 15:00:06 +0700

    update monster moving

--- src/Monster/GreenSlime.java
+++ src/Monster/GreenSlime.java
@@ -28,7 +28,6 @@ public class GreenSlime extends Entity {
         height = gp.tileSize;
         animationON = true ;
         getImage();
-        setAction();
     }
     public void getImage(){
         up1 = setup("/monster/greenslime_down_1" , width , height) ;
@@ -42,10 +41,59 @@ public class GreenSlime extends Entity {
     }
     public void update() {
         // no time to finish monster moving
+        setAction();
         spriteCounter++;
         if (spriteCounter > 12) {      // ~5 FPS animation nếu game 60 FPS
             spriteNum = (spriteNum == 1 ? 2 : 1);
             spriteCounter = 0;
+        }
+        int deltaMoveX = 0;
+        int deltaMoveY = 0;
+        double length = Math.sqrt(deltaMoveX * deltaMoveX + deltaMoveY * deltaMoveY);
+        if (length != 0) {
+            deltaMoveX = (int) Math.round(deltaMoveX / length * actualSpeed);
+            deltaMoveY = (int) Math.round(deltaMoveY / length * actualSpeed);
+        }
+        switch (direction) {
+            case "up":    deltaMoveY = -actualSpeed; break;
+            case "down":  deltaMoveY =  actualSpeed; break;
+            case "left":  deltaMoveX = -actualSpeed; break;
+            case "right": deltaMoveX =  actualSpeed; break;
+        }
+        /*
+        there is a problem , If the position touches the boundary, it is impossible
+        to press 2 keys to move diagonally and the result is to move in a
+        normal direction. therefore, valriable collisionOn is for static object and tiles.
+        if we use that val for both coordinates x and y , we can not move in the most
+        possible direction. I use 2 val called collisionXOn collisionYOn for the movement
+        of player so i can calculate the posible future move.
+
+        -> we have to predict the path that the player can move.
+        */
+        // collide with X
+        collisionOn = false;
+        collisionXOn = false;
+
+
+        int nextX = worldX + deltaMoveX; // posible move X
+        int nextY = worldY; // posible move Y
+        gp.cChecker.checkTile(this, nextX, nextY);
+        if (!collisionXOn && !collisionOn) {
+            worldX = nextX;
+            //System.out.print(worldX / 16 + " ");
+        }
+
+
+        //collide with Y
+        collisionOn = false;
+        collisionYOn = false;
+
+        nextX = worldX;
+        nextY = worldY + deltaMoveY;
+        gp.cChecker.checkTile(this, nextX, nextY);
+        if (!collisionYOn && !collisionOn) {
+            worldY = nextY;
+            //System.out.println(worldY / 16);
         }
     }
     public void setAction(){
